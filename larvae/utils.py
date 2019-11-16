2a04a99943d406517e5969af21b49002db245a9e
harshilkamdar <harshilkamdar@ymail.com>
2019-11-16 17:51:32 -0500

    oop

--- larvae/utils.py
+++ larvae/utils.py
@@ -1,9 +1,19 @@
 import numpy as np
 
-def add_noise_to_mock_spectra(spectra, snr=100, return_original=True):
+def add_noise_to_mock_spectra(spectra, snr=100):
     """
     add gaussian noise to spectra
-    """
-    noise = 1/SNR #assuming the flux is continuum-normalized??
-    noisy_spectra = np.zeros_like(spectra)
     
+    Parameters
+    ----------
+
+    spectra : (normalized flux  x  pixels) : numpy.ndarray
+    snr : desired signal-to-noise ratio  : int or numpy.array
+    """
+    noise = 1/snr #assuming the flux is continuum-normalized??
+    noise *= np.random.randn(spectra.shape[0], spectra.shape[1])
+    noisy_spectra = spectra + noise
+
+    return noisy_spectra, spectra
+
+
